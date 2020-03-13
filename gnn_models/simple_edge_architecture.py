4d3eda0634a6b347da99aa543dbc9e7392f55b9d
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-13 17:00:34 -0400

    Changed model.

--- gnn_models/simple_edge_architecture.py
+++ gnn_models/simple_edge_architecture.py
@@ -39,7 +39,7 @@ class Net(torch.nn.Module):
         self.conv5 = EdgeConv(dim, dim)
 
         # Final MLP for regression.
-        self.fc1 = Lin(1 * dim, dim)
+        self.fc1 = Lin(6 * dim, dim)
         self.fc2 = Lin(dim, dim)
         self.fc3 = Lin(dim, dim)
         self.fc4 = Lin(dim, 1)
@@ -60,8 +60,8 @@ class Net(torch.nn.Module):
         xs.append(F.relu(self.conv5(xs[-1], data.edge_index, data.edge_types)))
 
 
-        # x = torch.cat(xs[0:], dim=-1)
-        x = xs[-1]
+        x = torch.cat(xs[0:], dim=-1)
+        # x = xs[-1]
         x = x[data.assoc_var]
 
         x = F.relu(self.fc1(x))
