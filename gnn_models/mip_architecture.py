a008b27e8f104d934f37d2a836857019e916cc6b
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-16 15:12:18 -0400

    Started implementing bipartite model with domain knowledge.

--- gnn_models/mip_architecture.py
+++ gnn_models/mip_architecture.py
@@ -266,12 +266,12 @@ val_dataset = dataset[800:900].shuffle()
 test_dataset = dataset[900:].shuffle()
 
 train_loader = DataLoader(train_dataset, batch_size=5, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=50, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=50, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=5, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=5, shuffle=True)
 
 print("### DATA LOADED.")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = Net(dim=64).to(device)
+model = Net(dim=32).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
     optimizer, mode='min', factor=0.7, patience=5, min_lr=0.00001)
