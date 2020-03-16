3b15ec89a495f3ba968bd8c372160d9e0d029739
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-16 15:47:19 -0400

    Started implementing bipartite model with domain knowledge.

--- gnn_models/mip_architecture.py
+++ gnn_models/mip_architecture.py
@@ -104,9 +104,9 @@ class Net(torch.nn.Module):
         self.var_mlp = Seq(Lin(2, dim), ReLU(), Lin(dim, dim))
         self.con_mlp = Seq(Lin(2, dim), ReLU(), Lin(dim, dim))
 
-        self.conv1 = MIPGNN(dim, dim, 2, 1, root_weight=True, bias=True, num_bases=5)
-        self.conv2 = MIPGNN(dim, dim, 2, 1, root_weight=True, bias=True, num_bases=5)
-        self.conv3 = MIPGNN(dim, dim, 2, 1, root_weight=True, bias=True, num_bases=5)
+        self.conv1 = MIPGNN(dim, dim, 2, root_weight=True, bias=True, num_bases=5)
+        self.conv2 = MIPGNN(dim, dim, 2, root_weight=True, bias=True, num_bases=5)
+        self.conv3 = MIPGNN(dim, dim, 2, root_weight=True, bias=True, num_bases=5)
 
         # Final MLP for regression.
         self.fc1 = Lin(4 * dim, dim)
