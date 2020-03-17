aba84dabb459463b9e5de6c83db1c101376bfcb2
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-17 18:07:09 -0400

    Started implementing bipartite model with domain knowledge.

--- gnn_models/mip_architecture.py
+++ gnn_models/mip_architecture.py
@@ -108,8 +108,12 @@ class MIPGNN(MessagePassing):
         t_1 = torch.matmul(x[assoc_var], self.root_vars)
         t_2 = torch.matmul(x[assoc_con], self.root_vars)
 
-        new_out[assoc_var, 0:-1] +=  t_1
-        new_out[assoc_con, 0:-1] += t_2
+        s_1 = new_out[assoc_var, 0:-1] +  t_1
+        s_2 = new_out[assoc_con, 0:-1] + t_2
+
+        new_out[assoc_con, 0:-1] = s_1
+        new_out[assoc_var, 0:-1] = s_2
+
         aggr_out = new_out + self.bias
 
         return aggr_out
