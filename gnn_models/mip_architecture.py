ad9cd8767c573545b0b8818be38b76f1a31c0ae0
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-16 20:29:32 -0400

    Started implementing bipartite model with domain knowledge.

--- gnn_models/mip_architecture.py
+++ gnn_models/mip_architecture.py
@@ -26,28 +26,25 @@ from torch_geometric.nn.inits import uniform
 
 class MIPGNN(MessagePassing):
 
-    def __init__(self, in_channels, out_channels, num_relations, num_bases, **kwargs):
+    def __init__(self, in_channels, out_channels, **kwargs):
         super(MIPGNN, self).__init__(aggr='add', **kwargs)
 
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.num_relations = num_relations
-        self.num_bases = num_bases
-
         self.hidden_to_var = Seq(Lin(in_channels, in_channels), Sigmoid(), Lin(in_channels, 1))
 
-        self.basis = Param(torch.Tensor(num_bases, in_channels+1, out_channels))
-        self.att = Param(torch.Tensor(num_relations, num_bases))
-        self.root = Param(torch.Tensor(in_channels+1, out_channels+1))
+        self.w_cons = Param(torch.Tensor(in_channels+1, out_channels+1))
+        self.w_var = Param(torch.Tensor(in_channels+1, out_channels+1))
 
+        self.root = Param(torch.Tensor(in_channels+1, out_channels+1))
         self.bias = Param(torch.Tensor(out_channels+1))
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        size = self.num_bases * self.in_channels
-        uniform(size, self.basis)
-        uniform(size, self.att)
+        size = self.in_channels
+        uniform(size, self.w_cons)
+        uniform(size, self.w_cons)
         uniform(size, self.root)
         uniform(size, self.bias)
 
@@ -55,19 +52,13 @@ class MIPGNN(MessagePassing):
         return self.propagate(edge_index, size=size, x=x, edge_type=edge_type, edge_feature=edge_feature)
 
     def message(self, x_j, edge_index_j, edge_type, edge_feature, x):
-        w = torch.matmul(self.att, self.basis.view(self.num_bases, -1))
-
-
         # Split data.
         x_j_0 = x_j[edge_type == 0]
         x_j_1 = x_j[edge_type == 1]
 
-        w = w.view(self.num_relations, self.in_channels+1, self.out_channels)
-        w_0 = torch.index_select(w, 0, edge_type[edge_type == 0])
-        w_1 = torch.index_select(w, 0, edge_type[edge_type == 1])
 
-        out_0 = torch.bmm(x_j_0.unsqueeze(1), w_0).squeeze(-2)
-        out_1 = torch.bmm(x_j_1.unsqueeze(1), w_1).squeeze(-2)
+        out_0 = torch.bmm(x_j_0.unsqueeze(1), self.w_cons).squeeze(-2)
+        out_1 = torch.bmm(x_j_1.unsqueeze(1), self.w_var).squeeze(-2)
 
         # TODO: Check direction of message.
         c = edge_feature[edge_index_j][edge_type == 1]
@@ -108,10 +99,10 @@ class Net(torch.nn.Module):
         self.var_mlp = Seq(Lin(1, dim), ReLU(), Lin(dim, dim))
         self.con_mlp = Seq(Lin(1, dim), ReLU(), Lin(dim, dim))
 
-        self.conv1 = MIPGNN(dim, dim, 2, num_bases=5)
-        self.conv2 = MIPGNN(dim, dim, 2, num_bases=5)
-        self.conv3 = MIPGNN(dim, dim, 2, num_bases=5)
-        self.conv4 = MIPGNN(dim, dim, 2, num_bases=5)
+        self.conv1 = MIPGNN(dim, dim)
+        self.conv2 = MIPGNN(dim, dim)
+        self.conv3 = MIPGNN(dim, dim)
+
 
         # Final MLP for regression.
         self.fc1 = Lin(5 * (dim+1), dim)
