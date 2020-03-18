61326d64e7fffe6057b451bf24ffba8f7cf834ba
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-18 10:09:30 -0400

    Started implementing bipartite model with domain knowledge.

--- gnn_models/mip_architecture.py
+++ gnn_models/mip_architecture.py
@@ -158,7 +158,7 @@ class Net(torch.nn.Module):
         x = x[data.assoc_var]
 
         x = F.relu(self.fc1(x))
-        # x = F.dropout(x, p=0.5, training=self.training)
+        x = F.dropout(x, p=0.5, training=self.training)
         x = F.relu(self.fc2(x))
         # x = F.dropout(x, p=0.5, training=self.training)
         x = F.relu(self.fc3(x))
