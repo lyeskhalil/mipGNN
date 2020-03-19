09c726a6e6ca1718404300b3da89ba8e4ebbdd9c
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-19 19:27:11 -0400

    Implmented bipartite model with domain knowledge.  - Fixed problems with loss.

--- gnn_models/train_model_bipartite.py
+++ gnn_models/train_model_bipartite.py
@@ -178,10 +178,10 @@ def train():
     total_loss = 0
     total_loss_mae = 0
     loss = torch.nn.MSELoss()
-    mse = RMSELoss()
-    # mse = torch.nn.MSELoss()
+    #mse = RMSELoss()
+    mse = torch.nn.MSELoss()
     mae = torch.nn.L1Loss()
-    mse = torch.nn.SmoothL1Loss()
+    #mse = torch.nn.SmoothL1Loss()
 
     for data in train_loader:
         optimizer.zero_grad()
