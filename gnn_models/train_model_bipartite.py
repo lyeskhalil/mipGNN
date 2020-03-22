2af44a0d10dc395000f80ab186ef794ad6c0c8d6
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-22 13:59:21 -0400

    Work on bugfix.

--- gnn_models/train_model_bipartite.py
+++ gnn_models/train_model_bipartite.py
@@ -165,7 +165,7 @@ train_dataset = dataset[0:9000].shuffle()
 val_dataset = dataset[8000:9000].shuffle()
 test_dataset = dataset[9000:].shuffle()
 
-batch_size = 50
+batch_size = 20
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
