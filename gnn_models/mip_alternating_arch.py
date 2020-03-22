99bed57d737a79af5237b8863fa0683644eaf322
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-21 23:29:12 -0400

    Implmented bipartite model with domain knowledge.   - Small fix.

--- gnn_models/mip_alternating_arch.py
+++ gnn_models/mip_alternating_arch.py
@@ -47,7 +47,7 @@ class CONS_TO_VAR(MessagePassing):
         violation = x_j[:, -1]
         violation = hidden_to_var(x_i).view(-1) * violation * c.view(-1)
         #### TODO: FIX numerical problems here
-        # violation = c.view(-1) / asums_j * hidden_to_var(x_i).view(-1) * violation
+        violation = c.view(-1) / asums_j * hidden_to_var(x_i).view(-1) * violation
         #### TODO: revert
         # violation = torch.zeros(violation.size(0)).cuda()
 
