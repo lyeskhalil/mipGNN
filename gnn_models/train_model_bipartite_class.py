49bf181a01719c4b3aae2acf4082ef1bb0b3df7d
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-23 21:43:30 -0400

    Classification baseline.

--- gnn_models/train_model_bipartite_class.py
+++ gnn_models/train_model_bipartite_class.py
@@ -159,9 +159,9 @@ dataset = GISR(path, transform=MyTransform()).shuffle()
 len(dataset)
 
 
-train_dataset = dataset[0:8000].shuffle()
-val_dataset = dataset[8000:9000].shuffle()
-test_dataset = dataset[9000:].shuffle()
+train_dataset = dataset[0:80].shuffle()
+val_dataset = dataset[80:90].shuffle()
+test_dataset = dataset[90:100].shuffle()
 
 
 
@@ -174,9 +174,9 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 print("### DATA LOADED.")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net(dim=128).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-    optimizer, mode='min', factor=0.7, patience=3, min_lr=0.00001, )
+    optimizer, mode='min', factor=0.7, patience=3, min_lr=0.00001)
 print("### SETUP DONE.")
 
 
