5fed49f028381d309f1bad7ca762d2f9f837c134
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-26 09:42:37 -0400

    New datasets.

--- gnn_models/mip_alternating_arch.py
+++ gnn_models/mip_alternating_arch.py
@@ -217,9 +217,9 @@ class Net(torch.nn.Module):
         x = F.relu(self.fc1(x))
         # x = F.dropout(x, p=0.5, training=self.training)
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        x = F.relu(self.fc5(x))
+        # x = F.relu(self.fc3(x))
+        # x = F.relu(self.fc4(x))
+        # x = F.relu(self.fc5(x))
         #x = F.sigmoid(self.fc6(x))
         x = self.fc6(x)
 
