c685a3f52578bd48b69716f5ab38dc1e388397b1
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-27 19:26:55 -0400

    New datasets.

--- gnn_models/train_model_bipartite_class.py
+++ gnn_models/train_model_bipartite_class.py
@@ -164,6 +164,7 @@ train_dataset = dataset[0:800].shuffle()
 val_dataset = dataset[800:900].shuffle()
 test_dataset = dataset[900:1000].shuffle()
 
+
 print(1-test_dataset.data.y.sum().item()/test_dataset.data.y.size(-1))
 
 batch_size = 20
