a96f72f8173000c654acd8867345267c23206c6d
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-27 19:23:24 -0400

    New datasets.

--- gnn_models/mip_alternating_arch_class.py
+++ gnn_models/mip_alternating_arch_class.py
@@ -13,7 +13,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 class CONS_TO_VAR(MessagePassing):
     def __init__(self, in_channels, out_channels, **kwargs):
         # TODO: mean?
-        super(CONS_TO_VAR, self).__init__(aggr='mean', **kwargs)
+        super(CONS_TO_VAR, self).__init__(aggr='add', **kwargs)
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.mlp_cons = Seq(Lin(in_channels, in_channels - 1), ReLU(), Lin(in_channels - 1, in_channels - 1))
@@ -70,7 +70,7 @@ class CONS_TO_VAR(MessagePassing):
 class VARS_TO_CON(MessagePassing):
     def __init__(self, in_channels, out_channels, **kwargs):
         # TODO: mean?
-        super(VARS_TO_CON, self).__init__(aggr='mean', **kwargs)
+        super(VARS_TO_CON, self).__init__(aggr='add', **kwargs)
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.mlp_var = Seq(Lin(in_channels, in_channels - 1), ReLU(), Lin(in_channels - 1, in_channels - 1))
