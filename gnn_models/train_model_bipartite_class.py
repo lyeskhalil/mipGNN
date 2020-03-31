b898dd53280309f9b36e39f70032bef51de49e8b
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-03-30 21:32:52 -0400

    Adde inference example for doing inference from trained models.    - Added maps.

--- gnn_models/train_model_bipartite_class.py
+++ gnn_models/train_model_bipartite_class.py
@@ -27,12 +27,12 @@ class GISR(InMemoryDataset):
     @property
     def raw_file_names(self):
         # return "tedsfrffrssedrsst"
-        return "tedswfrffrsserererdrsst"
+        return "tedsgwfrffrsserererdrsst"
 
     @property
     def processed_file_names(self):
         # return "tessrfffdrdderfdssst"
-        return "tedwsfrffrssedrrrrrsst"
+        return "tedwsfgrffrssedrrrrrsst"
 
     def download(self):
         pass
@@ -40,8 +40,8 @@ class GISR(InMemoryDataset):
     def process(self):
         data_list = []
 
-        path = '../gisp_generator/DATA/er_200_SET2_1k/'
-        # path = '../gisp_generator/DATA/er_200_SET1/'
+        #path = '../gisp_generator/DATA/er_200_SET2_1k/'
+        path = '../gisp_generator/DATA/er_200_SET1/'
         total = len(os.listdir(path))
 
         for num, filename in enumerate(os.listdir(path)[0:1000]):
