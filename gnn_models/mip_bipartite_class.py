583c39d88534d254ade20c58fb0eb1e6a8ac54b9
Christopher Morris <christopher.morris@udo.edu>
2020-08-24 18:31:43 -0400

    Cont. implementing new model.    - simple baseline layer

--- gnn_models/mip_bipartite_class.py
+++ gnn_models/mip_bipartite_class.py
@@ -113,7 +113,7 @@ class ConVarBipartiteLayer(MessagePassing):
         self.edge_encoder = Sequential(Linear(edge_dim, dim), ReLU(), Linear(dim, dim), ReLU(),
                                        BN(dim))
 
-        self.joint_con_encoder = Sequential(Linear(dim+1, dim), ReLU(), Linear(dim, dim), ReLU(), BN(dim))
+        self.joint_con_encoder = Sequential(Linear(dim+1, dim-1), ReLU(), Linear(dim-1, dim-1), ReLU(), BN(dim-1))
 
         self.mlp = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim), ReLU(), BN(dim))
         self.eps = torch.nn.Parameter(torch.Tensor([0]))
@@ -124,7 +124,7 @@ class ConVarBipartiteLayer(MessagePassing):
         edge_embedding = self.edge_encoder(edge_attr)
 
         # TODO: change this somwe
-        joint_con = self.joint_con_encoder(torch.cat([source, error_con], dim=-1))
+        joint_con = torch.cat([self.joint_con_encoder(torch.cat([source, error_con], dim=-1)), error_con], dim=-1)
         tmp = self.propagate(edge_index, x=joint_con, error=error_con, edge_attr=edge_embedding, size=size)
 
         out = self.mlp((1 + self.eps) * target + tmp)
