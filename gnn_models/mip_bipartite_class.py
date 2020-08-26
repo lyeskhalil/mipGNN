359eb24e980b47c5ce70811aee283cfdec30e526
Christopher Morris <christopher.morris@udo.edu>
2020-08-26 18:02:38 -0400

    Cont. implementing new model.    - simple baseline layer

--- gnn_models/mip_bipartite_class.py
+++ gnn_models/mip_bipartite_class.py
@@ -452,8 +452,8 @@ class MyData(Data):
             return torch.tensor([self.num_nodes_con, self.num_nodes_var]).view(2, 1)
         # elif key in ['index']:
         #     return torch.tensor(self.num_nodes_con)
-        # elif key in ['index_var']:
-        #     return torch.tensor(self.num_nodes_var)
+        elif key in ['index_var']:
+             return torch.tensor(self.num_nodes_var)
         else:
             return 0
 
