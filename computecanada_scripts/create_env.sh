f399dfc29a632b19174a5d929541da7848cd0536
Elias Khalil <khalile2@gra-login1.graham.sharcnet>
2020-12-19 18:11:03 -0500

    submitit script

--- computecanada_scripts/create_env.sh
+++ computecanada_scripts/create_env.sh
@@ -13,7 +13,12 @@ pip install --no-index --upgrade pip
 pip install numpy --no-index
 pip install networkx --no-index
 
+pip install multiprocess
+pip install submitit
+
 pip install torch==1.6.0+cpu torchvision==0.7.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+
+pip install tensorboard
 
 pip install torch-scatter==latest+cpu -f https://pytorch-geometric.com/whl/torch-1.6.0.html
 pip install torch-sparse==latest+cpu -f https://pytorch-geometric.com/whl/torch-1.6.0.html
--- model_execution/slurm_train.py
+++ model_execution/slurm_train.py
@@ -1,3 +1,4 @@
+import spo_train
 import submitit
 
 def add(a, b):
@@ -5,10 +6,11 @@ def add(a, b):
 
 # executor is the submission interface (logs are dumped in the folder)
 executor = submitit.AutoExecutor(folder="log_test")
+print(executor.which())
 # set timeout in min, and partition for running the job
-executor.update_parameters(timeout_min=1, slurm_partition="rrg-khalile2")
-job = executor.submit(add, 5, 7)  # will compute add(5, 7)
+executor.update_parameters(additional_parameters={"account": "rrg-khalile2"}, timeout_min=719)
+job = executor.submit(spo_train.main, [])  # will compute add(5, 7)
 print(job.job_id)  # ID of your job
 
-output = job.result()  # waits for completion and returns output
-assert output == 12  # 5 + 7 = 12...  your addition was computed in the cluster
+#output = job.result()  # waits for completion and returns output
+#assert output == 12  # 5 + 7 = 12...  your addition was computed in the cluster
