839e001d4494acca233389df67d7433036744f9c
Elias Khalil <lyes@gatech.edu>
2020-12-19 18:20:15 -0500

    updated slurm_train

--- model_execution/slurm_train.py
+++ model_execution/slurm_train.py
@@ -4,13 +4,30 @@ import submitit
 def add(a, b):
     return a + b
 
-# executor is the submission interface (logs are dumped in the folder)
 executor = submitit.AutoExecutor(folder="log_test")
 print(executor.which())
-# set timeout in min, and partition for running the job
-executor.update_parameters(additional_parameters={"account": "rrg-khalile2"}, timeout_min=719)
-job = executor.submit(spo_train.main, [])  # will compute add(5, 7)
+
+executor.update_parameters(
+	additional_parameters={"account": "rrg-khalile2"}, 
+	timeout_min=719,
+	mem_per_cpu=16,
+	cpus_per_task=10)
+
+job = executor.submit(spo_train.main, 
+	[
+	'-method', 'spo',
+	'-data_train_dir', '../gisp_generator/SPO_DATA/spo_gisp_er/150_150/alpha_0.75_numFeat_10_biasnodes_100_biasedges_10_halfwidth_0.5_polydeg_2/train',
+	'-data_validation_dir', '../gisp_generator/SPO_DATA/spo_gisp_er/150_150/alpha_0.75_numFeat_10_biasnodes_100_biasedges_10_halfwidth_0.5_polydeg_2/valid',
+	'-output_dir', 'spo_torch_valid_polydeg2',
+	'-nn_poly_degree', '1',
+	'-nn_depth', '2',
+	'-nn_width', '50',
+	'-nn_lr_decay', '1',
+	'-nn_lr_init', '1e-2',
+	'-nn_reg', '0',
+	'-nn_batchsize', '10',
+	'-nn_poolsize', '10'
+	])  
 print(job.job_id)  # ID of your job
 
 #output = job.result()  # waits for completion and returns output
-#assert output == 12  # 5 + 7 = 12...  your addition was computed in the cluster
