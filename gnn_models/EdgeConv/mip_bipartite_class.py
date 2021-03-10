468074ad0ef60b9eae4d9851a247c62754badf79
Christopher Morris <christopher.morris@udo.edu>
2021-03-09 19:23:36 -0500

    e

--- gnn_models/EdgeConv/mip_bipartite_class.py
+++ gnn_models/EdgeConv/mip_bipartite_class.py
@@ -436,8 +436,8 @@ file_list = [
     # "../../DATA1/er_SET2/300_300/alpha_0.75_setParam_100/train/",
     # "../DATA1/er_SET2/300_300/alpha_0.25_setParam_100/train/",
     # "../DATA1/er_SET2/300_300/alpha_0.5_setParam_100/train/",
-    "../../DATA1/er_SET1/400_400/alpha_0.75_setParam_100/train/",
-    # "../DATA1/er_SET1/400_400/alpha_0.5_setParam_100/train/",
+    #"../../DATA1/er_SET1/400_400/alpha_0.75_setParam_100/train/",
+    "../../DATA1/er_SET1/400_400/alpha_0.5_setParam_100/train/",
     # "../DATA1/er_SET1/400_400/alpha_0.25_setParam_100/train/",
 ]
 
@@ -448,8 +448,8 @@ name_list = [
     #"er_SET2_300_300_alpha_0_75_setParam_100_train",
     # "er_SET2_300_300_alpha_0_25_setParam_100_train",
     # "er_SET2_300_300_alpha_0_5_setParam_100_train",
-    "er_SET1_400_400_alpha_0_75_setParam_100_train",
-    # "er_SET1_400_400_alpha_0_5_setParam_100_train",
+    #"er_SET1_400_400_alpha_0_75_setParam_100_train",
+    "er_SET1_400_400_alpha_0_5_setParam_100_train",
     # "er_SET1_400_400_alpha_0_25_setParam_100_train",
 ]
 
