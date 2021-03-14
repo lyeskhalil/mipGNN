9c68cd6a4032067233a7bfc940d5d3aa4f9e0109
Christopher Morris <christopher.morris@udo.edu>
2021-03-14 12:42:29 -0400

    Added hyperparameter opt. using val. set.

--- gnn_models/EdgeConv/mip_bipartite_simple_class.py
+++ gnn_models/EdgeConv/mip_bipartite_simple_class.py
@@ -93,10 +93,10 @@ class SimpleNet(torch.nn.Module):
         x_con = [con_node_features_0]
 
         for i in range(self.num_layers):
-            x_con.append(F.relu(self.layers_con[i](x_var[-1], x_con[-1], edge_index_var, edge_features_var,
+            x_con.append(F.relu(self.layers_var[i](x_var[-1], x_con[-1], edge_index_var, edge_features_var,
                                                    (num_nodes_var.sum(), num_nodes_con.sum()))))
 
-            x_var.append(F.relu(self.layers_var[-1](x_con[-1], x_var[-1], edge_index_con, edge_features_con,
+            x_var.append(F.relu(self.layers_con[-1](x_con[-1], x_var[-1], edge_index_con, edge_features_con,
                                                     (num_nodes_con.sum(), num_nodes_var.sum()))))
 
         x = torch.cat(x_var[:], dim=-1)
