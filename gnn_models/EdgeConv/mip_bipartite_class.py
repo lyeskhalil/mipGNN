f739dac6a6bee60b9c289a18816a52970aacdc4a
Christopher Morris <christopher.morris@udo.edu>
2021-03-17 16:32:23 -0400

    Added MLP baseline.

--- gnn_models/EdgeConv/mip_bipartite_class.py
+++ gnn_models/EdgeConv/mip_bipartite_class.py
@@ -474,8 +474,10 @@ results = []
 for i in range(5):
     r = []
 
+    print(i)
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = SimpleNet(hidden=64, num_layers=5, aggr = "mean").to(device)
+    model = SimpleNet(hidden=128, num_layers=5, aggr = "mean").to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
