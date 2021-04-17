ca96a684c9f628d68b5478b69dee590c7f4ac01d
Christopher Morris <christopher.morris@udo.edu>
2021-04-16 22:36:41 -0400

    Started implementing HO model for testing.

--- gnn_models/ho_gnn/test_ho.py
+++ gnn_models/ho_gnn/test_ho.py
@@ -61,6 +61,7 @@ for num, filename in enumerate(os.listdir(data_path)):
             if graph_new.nodes[v]["type"] == "VC":
                 print(graph.nodes[first]["bipartite"], graph.nodes[second]["bipartite"])
                 print(graph.nodes[n], graph.nodes[second])
+                print(n, second)
                 print(graph.nodes[n]["bipartite"], graph.nodes[second]["bipartite"])
 
                 matrices_vc_cc_1.append([num, graph_new.nodes[(n, second)]["num"]])
