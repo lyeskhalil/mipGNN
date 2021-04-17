9442895ba656445a9a9a53c35d8cc55fd68f9720
Christopher Morris <christopher.morris@udo.edu>
2021-04-17 16:06:37 -0400

    Started implementing HO model for testing.

--- gnn_models/ho_gnn/test_ho.py
+++ gnn_models/ho_gnn/test_ho.py
@@ -84,7 +84,7 @@ class GraphDataset(InMemoryDataset):
             c = 0
             for i, (u, v) in enumerate(graph.edges):
                 if graph.nodes[u]['bipartite'] == 0:
-                    graph_new.add_node((u, v), type="VC", first=u, second=v, num=c, feauture = [graph.nodes[u]['objcoeff'], graph.degree[u], graph.nodes[v]['rhs'],  graph.degree[v]])
+                    graph_new.add_node((u, v), type="VC", first=u, second=v, num=c, feauture = [graph.nodes[u]['objcoeff'], graph.degree[u], graph.nodes[v]['rhs'],  graph.degree[v], graph.edges[(u,v)]["coeff"]])
                     c += 1
                     graph_new.add_node((v, u), type="CV", first=v, second=u, num=c, feauture = [graph.nodes[v]['rhs'],  graph.degree[v], graph.nodes[u]['objcoeff'], graph.degree[u]])
                     c += 1
