37901fdfababe7e80407eeb393250ecb0e261f6c
Christopher Morris <christopher.morris@udo.edu>
2021-04-17 14:23:58 -0400

    Started implementing HO model for testing.

--- gnn_models/ho_gnn/test_ho.py
+++ gnn_models/ho_gnn/test_ho.py
@@ -61,12 +61,7 @@ for num, filename in enumerate(os.listdir(data_path)):
 
             if graph_new.nodes[v]["type"] == "VV":
                 if graph.has_edge(n, second):
-                    print(graph.nodes[first], graph.nodes[n]),
-                    print(graph.nodes[first], graph.nodes[second]),
-                    print(graph.has_edge(n, second))
-                    print(graph.has_edge(second, n))
-
-                    print(graph_new.nodes[(n, second)])
+                    matrices_vv_cv_1.append([num, graph_new.nodes[(n, second)]["num"]])
             if graph_new.nodes[v]["type"] == "CC":
                 if graph.has_edge(n, second):
                     matrices_cc_vc_1.append([num, graph_new.nodes[(n, second)]["num"]])
