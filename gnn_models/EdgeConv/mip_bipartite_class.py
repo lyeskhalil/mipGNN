775bd21750cbad0bc7eb8df54bcb25062fb0dc63
Christopher Morris <christopher.morris@udo.edu>
2021-04-17 16:22:29 -0400

    Started implementing HO model for testing.

--- gnn_models/EdgeConv/mip_bipartite_class.py
+++ gnn_models/EdgeConv/mip_bipartite_class.py
@@ -322,10 +322,8 @@ class GraphDataset(InMemoryDataset):
                     edge_list_var.append([node_to_varnode[s], node_to_connode[t]])
                     edge_features_var.append([edge_data['coeff']])
 
-            print(edge_list_var)
-            exit()
+         
             edge_index_var = torch.tensor(edge_list_var).t().contiguous()
-            print(edge_index_var.size())
             edge_index_con = torch.tensor(edge_list_con).t().contiguous()
 
             # Create data object.
