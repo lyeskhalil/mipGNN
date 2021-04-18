d24cb3549d0c988e64c01aa9e595149697197e93
Christopher Morris <christopher.morris@udo.edu>
2021-04-17 20:23:52 -0400

    Started implementing HO model for testing.

--- gnn_models/ho_gnn/test_ho.py
+++ gnn_models/ho_gnn/test_ho.py
@@ -93,9 +93,10 @@ class SimpleNet(torch.nn.Module):
 
 
         print(edge_index_vv_cv_1[0,:].max(), edge_index_vv_cv_1[1,:].max(),  vv_0.size(), cv_0.size())
-        exit()
+
 
         self.vv_cv_1(vv_0, cv_0, edge_index_vv_cv_1, [num_nodes_vv.sum(), num_nodes_cv.sum()])
+        exit()
 
         print("###")
         exit()
