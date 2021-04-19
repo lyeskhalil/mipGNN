fc4c276edab3fb83ffcc05178feb6505a9e802af
Christopher Morris <christopher.morris@udo.edu>
2021-04-18 20:20:25 -0400

    Started implementing HO model for testing.

--- gnn_models/ho_gnn/mip_bipartite_simple_class.py
+++ gnn_models/ho_gnn/mip_bipartite_simple_class.py
@@ -364,7 +364,7 @@ results = []
 models = []
 for i in range(5):
     # TODO
-    models.append(SimpleNet(hidden=32, num_layers=1, aggr="add"))
+    models.append(SimpleNet(hidden=32, num_layers=1, aggr="mean"))
 
 
 for i in range(5):
