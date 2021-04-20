f804c3893f839c46212e3f73907d7e4720077322
Christopher Morris <christopher.morris@udo.edu>
2021-04-20 19:58:32 -0400

    Started implementing HO model for testing.

--- gnn_models/ho_gnn/test_ho_connected_g.py
+++ gnn_models/ho_gnn/test_ho_connected_g.py
@@ -206,7 +206,7 @@ class GraphDataset(InMemoryDataset):
                         if graph.nodes[u]['bipartite'] == 0 and graph.nodes[v]['bipartite'] == 0:
                             graph_new.add_node((u, v), type="VV", first=u, second=v, num=num)
 
-                            if i == j:
+                            if i == j or not graph.has_edge(u,v):
                                 features.append(
                                     [graph.nodes[u]['objcoeff'], 0, graph.degree[u], graph.nodes[v]['objcoeff'], 0,
                                      graph.degree[v], 0])
