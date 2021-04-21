6564580c55986b7dda456b8f3b03d25e61014650
Christopher Morris <christopher.morris@udo.edu>
2021-04-21 11:11:20 -0400

    Started implementing HO model for testing.

--- gnn_models/ho_gnn/test_ho_connected_g.py
+++ gnn_models/ho_gnn/test_ho_connected_g.py
@@ -350,7 +350,7 @@ val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = SimpleNet(hidden=64, aggr="mean").to(device)
+model = SimpleNet(hidden=32, aggr="mean").to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
                                                        factor=0.8, patience=10,
