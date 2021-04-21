592f6603cd9fa36ec77bac292ce243385f86f90c
Christopher Morris <christopher.morris@udo.edu>
2021-04-21 12:41:48 -0400

    Started implementing HO model for testing.

--- gnn_models/EdgeConv/mip_bipartite_regr_test.py
+++ gnn_models/EdgeConv/mip_bipartite_regr_test.py
@@ -444,8 +444,7 @@ def train(epoch):
         optimizer.zero_grad()
         data = data.to(device)
         out = model(data)
-        print(out)
-        exit()
+
 
         loss = torch.nn.MSELoss(out, data.y)
 
