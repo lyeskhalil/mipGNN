c8b9bc96a4344e31bfc9e60269da58b1c5032a60
Christopher Morris <christopher.morris@udo.edu>
2021-05-12 15:41:44 -0400

    Started implementation.

--- gnn_models/EdgeConv/mip_bipartite_regr.py
+++ gnn_models/EdgeConv/mip_bipartite_regr.py
@@ -476,7 +476,7 @@ l = len(val_index)
 
 
 val_dataset = train_dataset[val_index].shuffle()
-train_dataset = train_dataset[train_index].shuffle()
+train_dataset = torch.clone(train_dataset[train_index].shuffle())
 test_dataset = test_dataset[0:200].shuffle()
 
 print(val_dataset.data.y_real.mean())
