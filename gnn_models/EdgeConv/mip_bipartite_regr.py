3de0dfb258b3125ac70c32babd1115aaa0ef8419
Christopher Morris <christopher.morris@udo.edu>
2021-05-12 20:11:22 -0400

    Started implementation.

--- gnn_models/EdgeConv/mip_bipartite_regr.py
+++ gnn_models/EdgeConv/mip_bipartite_regr.py
@@ -491,7 +491,7 @@ if log:
     #val_dataset.data.y_real = torch.exp(val_dataset.data.y_real) - eps
 
 print(train_dataset.data.y_real.min(), train_dataset.data.y_real.mean(), train_dataset.data.y_real.max())
-plt.hist(train_dataset.data.y_real.cpu().numpy(), bins=20)
+plt.hist(train_dataset.data.y_real.cpu().numpy(), bins=1000)
 plt.show()
 plt.savefig('plot_trans.png')
 
