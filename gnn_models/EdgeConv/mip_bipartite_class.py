336b5b8e822e0f3c9dcf09b6dd54518b6f3a6abc
Christopher Morris <christopher.morris@udo.edu>
2021-05-12 21:25:43 -0400

    Started implementation.

--- gnn_models/EdgeConv/mip_bipartite_class.py
+++ gnn_models/EdgeConv/mip_bipartite_class.py
@@ -542,7 +542,7 @@ scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
                                                        factor=0.8, patience=6,
                                                        min_lr=0.0000001)
 
-for epoch in range(1, 50):
+for epoch in range(1, 20):
     print(i)
 
     train_loss, _ = train(epoch)
