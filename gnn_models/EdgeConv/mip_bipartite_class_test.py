18f38ce33768f08043642f705371991fb700d079
Christopher Morris <christopher.morris@udo.edu>
2021-05-14 12:54:55 -0400

    Adapted predict.py to current model.

--- gnn_models/EdgeConv/mip_bipartite_class_test.py
+++ gnn_models/EdgeConv/mip_bipartite_class_test.py
@@ -128,8 +128,8 @@ class SimpleNet(torch.nn.Module):
         self.num_layers = num_layers
 
         # Embed initial node features.
-        self.var_node_encoder = Sequential(Linear(1, hidden), ReLU(), Linear(hidden, hidden))
-        self.con_node_encoder = Sequential(Linear(1, hidden), ReLU(), Linear(hidden, hidden))
+        self.var_node_encoder = Sequential(Linear(2, hidden), ReLU(), Linear(hidden, hidden))
+        self.con_node_encoder = Sequential(Linear(2, hidden), ReLU(), Linear(hidden, hidden))
 
         # Compute variable assignement.
         self.layers_ass = []
