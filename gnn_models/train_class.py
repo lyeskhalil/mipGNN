28f9eb0ddc64ad5f61f2c4191deac940cdf22c17
Christopher Morris <christopher.morris@udo.edu>
2021-05-15 11:42:50 -0400

    Clean up.

--- gnn_models/train_class.py
+++ gnn_models/train_class.py
@@ -264,166 +264,168 @@ name_list = [
     "L_n200_p0.02_c500_test"
 ]
 
-i = int(sys.argv[1])
-m = sys.argv[2]
-bias = float(sys.argv[3])
+# i = int(sys.argv[1])
+# m = sys.argv[2]
+# bias = float(sys.argv[3])
+
+
+test_scores = []
+
+for i in [0, 2, 4, 6, 8, 10]:
+    for bias in [0.0, 0.001, 0.1]:
+        for m in ["EC", "ECS", "GIN", "GINS", "SG", "SGS"]:
+
+            # Setup model.
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+            if m == "EC":
+                model = EdgeConv(hidden=64, num_layers=4, aggr="mean").to(device)
+                model_name = "EC_" + name_list[i] + str(bias)
+                print(model_name, bias)
+            elif m == "ECS":
+                model = EdgeConvSimple(hidden=64, num_layers=4, aggr="mean").to(device)
+                model_name = "ECS_" + name_list[i] + str(bias)
+                print(model_name, bias)
+            elif m == "GIN":
+                model = GIN(hidden=64, num_layers=4, aggr="mean").to(device)
+                model_name = "GIN_" + name_list[i] + str(bias)
+                print(model_name, bias)
+            elif m == "GINS":
+                model = GINSimple(hidden=64, num_layers=4, aggr="mean").to(device)
+                model_name = "GINS_" + name_list[i] + str(bias)
+                print(model_name, bias)
+            elif m == "SG":
+                model = Sage(hidden=64, num_layers=4, aggr="mean").to(device)
+                model_name = "SG_" + name_list[i] + str(bias)
+                print(model_name, bias)
+            elif m == "SGS":
+                model = SageSimple(hidden=64, num_layers=4, aggr="mean").to(device)
+                model_name = "SGS_" + name_list[i] + str(bias)
+                print(model_name, bias)
+
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
+                                                                   factor=0.8, patience=10,
+                                                                   min_lr=0.0000001)
+
+            # Prepare data.
+            bias_threshold = bias
+            batch_size = 5
+            num_epochs = 30
+
+
+            pathr = osp.join(osp.dirname(osp.realpath(__file__)), '.', 'data', 'DS')
+
+            pd = path_train = path_trainpath_train = dataset_list[i]
+            name = name_train = name_list[i]
+            train_dataset = GraphDataset(name_train, pathr, path_train, bias_threshold, transform=MyTransform()).shuffle()
+
+            pd = path_test = path_testpath_test = dataset_list[i + 1]
+            name = name_test = name_list[i + 1]
+            test_dataset = GraphDataset(name_test, pathr, path_test, bias_threshold, transform=MyTransform()).shuffle()
+
+            train_index, val_index = train_test_split(list(range(0, len(train_dataset))), test_size=0.2)
+            val_dataset = train_dataset[val_index].shuffle()
+            train_dataset = train_dataset[train_index].shuffle()
+            test_dataset = test_dataset.shuffle()
+
+            train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+            val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+            test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+
+            def train(epoch):
+                model.train()
+
+                loss_all = 0
+                zero = torch.tensor([0]).to(device)
+                one = torch.tensor([1]).to(device)
+
+                for data in train_loader:
+                    data = data.to(device)
+
+                    y = data.y_real
+                    y = torch.where(y <= bias_threshold, zero, one).to(device)
+
+                    optimizer.zero_grad()
+                    output = model(data)
+
+                    loss = F.nll_loss(output, y)
+                    loss.backward()
+                    loss_all += batch_size * loss.item()
+                    optimizer.step()
+
+                return loss_all / len(train_dataset)
+
+
+            @torch.no_grad()
+            def test(loader):
+                model.eval()
+
+                zero = torch.tensor([0]).to(device)
+                one = torch.tensor([1]).to(device)
+                f1 = F1(num_classes=2, average="macro").to(device)
+                pr = Precision(num_classes=2, average="macro").to(device)
+                re = Recall(num_classes=2, average="macro").to(device)
+                acc = Accuracy(num_classes=2).to(device)
+
+                first = True
+                for data in loader:
+                    data = data.to(device)
+                    pred = model(data)
+
+                    y = data.y_real
+
+                    y = torch.where(y <= bias_threshold, zero, one).to(device)
+                    pred = pred.max(dim=1)[1]
+
+                    if not first:
+                        pred_all = torch.cat([pred_all, pred])
+                        y_all = torch.cat([y_all, y])
+                    else:
+                        pred_all = pred
+                        y_all = y
+                        first = False
+
+                return acc(pred_all, y_all), f1(pred_all, y_all), pr(pred_all, y_all), re(pred_all, y_all)
+
+
+            best_val = 0.0
+            test_acc = 0.0
+            test_f1 = 0.0
+            test_re = 0.0
+            test_pr = 0.0
+            for epoch in range(1, num_epochs+1):
+                print(i)
+
+                train_loss = train(epoch)
+                train_acc, train_f1, train_pr, train_re = test(train_loader)
+
+                val_acc, val_f1, val_pr, val_re = test(val_loader)
+                scheduler.step(val_acc)
+                lr = scheduler.optimizer.param_groups[0]['lr']
+
+                if val_acc > best_val:
+                    best_val = val_acc
+                    test_acc, test_f1, test_pr, test_re = test(test_loader)
+                    torch.save(model.state_dict(), model_name)
+
+                # Break if learning rate is smaller 10**-6.
+                if lr < 0.000001 or epoch == num_epochs:
+                    test_scores.append([model_name, test_acc, test_f1, test_pr, test_re])
+
+                    break
 
 
 
-
-# Setup model.
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-if m == "EC":
-    model = EdgeConv(hidden=64, num_layers=4, aggr="mean").to(device)
-    model_name = "EC_" + name_list[i] + str(bias)
-    print(model_name, bias)
-elif m == "ECS":
-    model = EdgeConvSimple(hidden=64, num_layers=4, aggr="mean").to(device)
-    model_name = "ECS_" + name_list[i] + str(bias)
-    print(model_name, bias)
-elif m == "GIN":
-    model = GIN(hidden=64, num_layers=4, aggr="mean").to(device)
-    model_name = "GIN_" + name_list[i] + str(bias)
-    print(model_name, bias)
-elif m == "GINS":
-    model = GINSimple(hidden=64, num_layers=4, aggr="mean").to(device)
-    model_name = "GINS_" + name_list[i] + str(bias)
-    print(model_name, bias)
-elif m == "SG":
-    model = Sage(hidden=64, num_layers=4, aggr="mean").to(device)
-    model_name = "SG_" + name_list[i] + str(bias)
-    print(model_name, bias)
-elif m == "SGS":
-    model = SageSimple(hidden=64, num_layers=4, aggr="mean").to(device)
-    model_name = "SGS_" + name_list[i] + str(bias)
-    print(model_name, bias)
-
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
-                                                       factor=0.8, patience=10,
-                                                       min_lr=0.0000001)
+                # print('Epoch: {:03d}, LR: {:.7f}, Train Loss: {:.7f},  '
+                #       'Train Acc: {:.7f}, Val Acc: {:.7f}, Test Acc: {:.7f}'.format(epoch, lr, train_loss,
+                #                                                                     train_acc, val_acc, test_acc))
+                #
+                # print("F1", train_f1, val_f1, test_f1)
+                # print("Pr", train_pr, val_pr, test_pr)
+                # print("Re", train_re, val_re, test_re)
 
 
-
-
-# Prepare data.
-bias_threshold = bias
-batch_size = 5
-num_epochs = 30
-
-
-
-for i in range(22):
-    print(name_list[i])
-
-    pathr = osp.join(osp.dirname(osp.realpath(__file__)), '.', 'data', 'DS')
-
-    pd = path_train = path_trainpath_train = dataset_list[i]
-    name = name_train = name_list[i]
-    train_dataset = GraphDataset(name_train, pathr, path_train, bias_threshold, transform=MyTransform()).shuffle()
-
-    # pd = path_test = path_testpath_test = dataset_list[i + 1]
-    # name = name_test = name_list[i + 1]
-    # test_dataset = GraphDataset(name_test, pathr, path_test, bias_threshold, transform=MyTransform()).shuffle()
-
-exit()
-
-train_index, val_index = train_test_split(list(range(0, len(train_dataset))), test_size=0.2)
-val_dataset = train_dataset[val_index].shuffle()
-train_dataset = train_dataset[train_index].shuffle()
-test_dataset = test_dataset.shuffle()
-
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
-
-
-def train(epoch):
-    model.train()
-
-    loss_all = 0
-    zero = torch.tensor([0]).to(device)
-    one = torch.tensor([1]).to(device)
-
-    for data in train_loader:
-        data = data.to(device)
-
-        y = data.y_real
-        y = torch.where(y <= bias_threshold, zero, one).to(device)
-
-        optimizer.zero_grad()
-        output = model(data)
-
-        loss = F.nll_loss(output, y)
-        loss.backward()
-        loss_all += batch_size * loss.item()
-        optimizer.step()
-
-    return loss_all / len(train_dataset)
-
-
-@torch.no_grad()
-def test(loader):
-    model.eval()
-
-    zero = torch.tensor([0]).to(device)
-    one = torch.tensor([1]).to(device)
-    f1 = F1(num_classes=2, average="macro").to(device)
-    pr = Precision(num_classes=2, average="macro").to(device)
-    re = Recall(num_classes=2, average="macro").to(device)
-    acc = Accuracy(num_classes=2).to(device)
-
-    first = True
-    for data in loader:
-        data = data.to(device)
-        pred = model(data)
-
-        y = data.y_real
-
-        y = torch.where(y <= bias_threshold, zero, one).to(device)
-        pred = pred.max(dim=1)[1]
-
-        if not first:
-            pred_all = torch.cat([pred_all, pred])
-            y_all = torch.cat([y_all, y])
-        else:
-            pred_all = pred
-            y_all = y
-            first = False
-
-    return acc(pred_all, y_all), f1(pred_all, y_all), pr(pred_all, y_all), re(pred_all, y_all)
-
-
-best_val = 0.0
-test_acc = 0.0
-test_f1 = 0.0
-test_re = 0.0
-test_pr = 0.0
-for epoch in range(1, num_epochs):
-    print(i)
-
-    train_loss = train(epoch)
-    train_acc, train_f1, train_pr, train_re = test(train_loader)
-
-    val_acc, val_f1, val_pr, val_re = test(val_loader)
-    scheduler.step(val_acc)
-    lr = scheduler.optimizer.param_groups[0]['lr']
-
-    if val_acc > best_val:
-        best_val = val_acc
-        test_acc, test_f1, test_pr, test_re = test(test_loader)
-        torch.save(model.state_dict(), model_name)
-
-    # Break if learning rate is smaller 10**-6.
-    if lr < 0.000001:
-        break
-
-    print('Epoch: {:03d}, LR: {:.7f}, Train Loss: {:.7f},  '
-          'Train Acc: {:.7f}, Val Acc: {:.7f}, Test Acc: {:.7f}'.format(epoch, lr, train_loss,
-                                                                        train_acc, val_acc, test_acc))
-
-    print("F1", train_f1, val_f1, test_f1)
-    print("Pr", train_pr, val_pr, test_pr)
-    print("Re", train_re, val_re, test_re)
+print(test_scores)
