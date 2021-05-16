47db0000703847822df63822e55b1d7f3e690c9d
Christopher Morris <christopher.morris@udo.edu>
2021-05-15 21:04:36 -0400

    Clean up. Pushed evaluation script for regression.

--- gnn_models/GIN/mip_bipartite_simple_class.py
+++ gnn_models/GIN/mip_bipartite_simple_class.py
@@ -49,8 +49,8 @@ class SimpleNet(torch.nn.Module):
         self.regression = regression
 
         # Embed initial node features.
-        self.var_node_encoder = Sequential(Linear(1, hidden), ReLU(), Linear(hidden, hidden))
-        self.con_node_encoder = Sequential(Linear(1, hidden), ReLU(), Linear(hidden, hidden))
+        self.var_node_encoder = Sequential(Linear(2, hidden), ReLU(), Linear(hidden, hidden))
+        self.con_node_encoder = Sequential(Linear(2, hidden), ReLU(), Linear(hidden, hidden))
 
         # Bipartite GNN architecture.
         self.layers_con = []
