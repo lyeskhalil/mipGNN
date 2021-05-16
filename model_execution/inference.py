f0353821767ea68af27d58fb5b9aecd8e04d2761
Elias Khalil <lyes@gatech.edu>
2021-05-16 16:01:50 -0400

    mipeval submission code

--- model_execution/inference.py
+++ model_execution/inference.py
@@ -59,40 +59,37 @@ def set_cplex_priorities(instance_cpx, prediction):
     instance_cpx.order.set(order_tuples)
     # print(instance_cpx.order.get())
 
-if __name__ == '__main__':
+def mipeval(
+    method, 
+    instance, 
+    graph='', 
+    model='', 
+    logfile='sys.stdout',
+    barebones=0,
+    timelimit=60,
+    memlimit=1024,
+    freq_best=100,
+    lb_threshold=5
+    ):
 
-    """ Parse arguments """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-method", type=str, default='default')
-    parser.add_argument("-instance", type=str, default='../gisp_generator/LP/er_200_SET2_1k/er_n=200_m=1867_p=0.10_SET2_setparam=100.00_alpha=0.75_606.lp')
-    parser.add_argument("-graph", type=str, default='')
-    parser.add_argument("-model", type=str, default='../gnn_models/trained_model_er_200_SET2_1k_new')
-    parser.add_argument("-barebones", type=int, default=1)
-    parser.add_argument("-timelimit", type=float, default=60)
-    parser.add_argument("-logfile", type=str, default='sys.stdout')
-
-    # Parameters for exact local branching
-    parser.add_argument("-lb_threshold", type=int, default=5)
-
-    # Parameters for primal heuristic mip start
-    parser.add_argument("-rounding_threshold", type=float, default=0.1)
-
-    args = parser.parse_args()
-    print(args)
-
-    instance_path_split = args.instance.split('/')
+    instance_path_split = instance.split('/')
     instance_name = instance_path_split[-2] + '/' + instance_path_split[-1][:-3]
 
     """ Create CPLEX instance """
-    instance_cpx = cplex.Cplex(args.instance)
+    instance_cpx = cplex.Cplex(instance)
+    start_time = instance_cpx.get_time()
     # instance_cpx = cplex.Cplex("../gisp_generator/LP/" + instance_name + ".lp")
 
     """ Set CPLEX parameters, if any """
-    instance_cpx.parameters.timelimit.set(args.timelimit)
+    instance_cpx.parameters.timelimit.set(timelimit)
     instance_cpx.parameters.emphasis.mip.set(1)
     instance_cpx.parameters.mip.display.set(3)
     instance_cpx.parameters.threads.set(1)
-    if args.barebones:
+    instance_cpx.parameters.workmem.set(memlimit)
+    instance_cpx.parameters.mip.limits.treememory.set(20000)
+    instance_cpx.parameters.mip.strategy.file.set(2)
+    instance_cpx.parameters.workdir.set("./cpx_tmp/")
+    if barebones:
         instance_cpx.parameters.mip.limits.cutpasses.set(-1)
         instance_cpx.parameters.mip.strategy.heuristicfreq.set(-1)
         instance_cpx.parameters.preprocessing.presolve.set(0)
@@ -101,11 +98,11 @@ if __name__ == '__main__':
         # instance_cpx.parameters.mip.strategy.nodeselect.set(3)
 
     """ Solve CPLEX instance with user-selected method """
-    if 'default' not in args.method:
+    if 'default' not in method:
         """ Read in the pickled graph and the trained model """
         # graph = nx.read_gpickle("../gisp_generator/DATA/" + instance_name + ".pk")
-        graph = nx.read_gpickle(args.graph)
-        prediction, node_to_varnode = predict.get_prediction(model_name=args.model, graph=graph)
+        graph = nx.read_gpickle(graph)
+        prediction, node_to_varnode = predict.get_prediction(model_name=model, graph=graph)
         dict_varname_seqid = predict.get_variable_cpxid(graph, node_to_varnode, prediction)
         # print(prediction)
         # todo check dimensions of p
@@ -115,7 +112,7 @@ if __name__ == '__main__':
         prediction_reord = [dict_varname_seqid[var_name][1] for var_name in var_names]
         prediction = np.array(prediction_reord)
 
-        if 'local_branching' in args.method:
+        if 'local_branching' in method:
             pred_one_coeff = (prediction >= 0.9) * (-1)
             pred_zero_coeff = (prediction <= 0.1)
             num_ones = -np.sum(pred_one_coeff)
@@ -123,24 +120,24 @@ if __name__ == '__main__':
 
             local_branching_coeffs = [list(range(len(prediction))), coeffs.tolist()]
 
-        elif args.method == 'branching_priorities':
+        elif method == 'branching_priorities':
             set_cplex_priorities(instance_cpx, prediction)
 
-        elif args.method == 'local_branching_approx':
+        elif method == 'local_branching_approx':
             instance_cpx.linear_constraints.add(
                 lin_expr=[local_branching_coeffs],
                 senses=['L'],
-                rhs=[float(args.lb_threshold - num_ones)],
+                rhs=[float(lb_threshold - num_ones)],
                 names=['local_branching'])
         
-        elif args.method == 'local_branching_exact':
+        elif method == 'local_branching_exact':
             branch_cb = instance_cpx.register_callback(callbacks_cplex.branch_local_exact)
 
             branch_cb.coeffs = local_branching_coeffs
-            branch_cb.threshold = args.lb_threshold - num_ones
+            branch_cb.threshold = lb_threshold - num_ones
             branch_cb.is_root = True
 
-        elif 'node_selection' in args.method:
+        elif 'node_selection' in method:
             # score variables based on bias prediction
             scores = np.max(((1-prediction), prediction), axis=0)
             rounding = np.round(prediction)
@@ -153,7 +150,7 @@ if __name__ == '__main__':
             branch_cb.rounding = rounding
 
             node_cb.last_best = 0
-            node_cb.freq_best = 100
+            node_cb.freq_best = freq_best
 
             node_priority = []
             branch_cb.node_priority = node_priority
@@ -162,10 +159,10 @@ if __name__ == '__main__':
             branch_cb.time = 0
             node_cb.time = 0
 
-            if 'branching' in args.method:
+            if 'branching' in method:
                 set_cplex_priorities(instance_cpx, prediction)
 
-        elif args.method == 'primal_mipstart':
+        elif method == 'primal_mipstart':
             # instance_cpx.parameters.mip.limits.nodes.set(1)
 
             # threshold_set = np.minimum(prediction, 1-prediction)
@@ -173,7 +170,7 @@ if __name__ == '__main__':
 
             threshold_set = [0.01, 0.05, 0.1, 0.2, 0.4, 0.5]
 
-            # threshold = args.rounding_threshold
+            # threshold = rounding_threshold
             for threshold in threshold_set:
                 indices_integer = np.where((prediction >= 1-threshold) | (prediction <= threshold))[0]
                 print(len(indices_integer), len(prediction))
@@ -184,13 +181,13 @@ if __name__ == '__main__':
                         val=np.round(prediction[indices_integer]).tolist()),
                     instance_cpx.MIP_starts.effort_level.repair)
 
-    if args.method == 'default_emptycb':
+    if method == 'default_emptycb':
         branch_cb = instance_cpx.register_callback(callbacks_cplex.branch_empty)
 
     """ CPLEX output management """
     logstring = sys.stdout
     summary_string = sys.stdout
-    if args.logfile != 'sys.stdout':
+    if logfile != 'sys.stdout':
         logstring = io.StringIO()
         summary_string = io.StringIO()
         instance_cpx.set_log_stream(logstring)
@@ -201,8 +198,6 @@ if __name__ == '__main__':
 
     # todo: consider runseeds 
     #  https://www.ibm.com/support/knowledgecenter/SSSA5P_12.9.0/ilog.odms.cplex.help/refpythoncplex/html/cplex.Cplex-class.html?view=kc#runseeds
-    start_time = instance_cpx.get_time()
-    # if 'primal_' not in args.method:            
     instance_cpx.solve()
     end_time = instance_cpx.get_time()
 
@@ -224,10 +219,37 @@ if __name__ == '__main__':
     else:
         summary_string.write('solving stats,no solutions found\n')
 
-    if args.logfile != 'sys.stdout':
+    if logfile != 'sys.stdout':
         if instance_cpx.solution.is_primal_feasible():
             _, incumbent_str = utils.parse_cplex_log(logstring.getvalue())
             summary_string.write(incumbent_str)
         summary_string = summary_string.getvalue()
-        with open(args.logfile, 'w') as logfile:
+        with open(logfile, 'w') as logfile:
             logfile.write(summary_string)
+
+if __name__ == '__main__':
+
+    """ Parse arguments """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-method", type=str, default='default')
+    parser.add_argument("-instance", type=str)
+    parser.add_argument("-graph", type=str, default='')
+    parser.add_argument("-model", type=str, default="../gnn_models/EdgeConv/trained_p_hat300-2")
+    parser.add_argument("-barebones", type=int, default=0)
+    parser.add_argument("-timelimit", type=float, default=60)
+    parser.add_argument("-memlimit", type=float, default=1024)
+    parser.add_argument("-logfile", type=str, default='sys.stdout')
+
+    # Parameters for node selection
+    parser.add_argument("-freq_best", type=int, default=100)
+
+    # Parameters for exact local branching
+    parser.add_argument("-lb_threshold", type=int, default=5)
+
+    # Parameters for primal heuristic mip start
+    # parser.add_argument("-rounding_threshold", type=float, default=0.1)
+
+    args = parser.parse_args()
+    print(args)
+
+    mipeval(**vars(args))
