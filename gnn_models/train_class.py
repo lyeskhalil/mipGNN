2345dd09cb96fd4d795be43300c48ae1a196d89c
Christopher Morris <christopher.morris@udo.edu>
2021-05-16 14:38:42 -0400

    Clean up. Pushed evaluation script for regression.

--- gnn_models/train_class.py
+++ gnn_models/train_class.py
@@ -271,12 +271,16 @@ name_list = [
 
 test_scores = []
 
+
 # Datasets.
-for i in [0, 2, 4, 6, 8, 10]:
+# for i in [0, 2, 4, 6, 8, 10]:
+for i in [0]:
     # Bias.
-    for bias in [0.0, 0.001, 0.1]:
+    # for bias in [0.0, 0.001, 0.1]:
+    for bias in [0.1]:
         # GNN.
-        for m in ["EC", "ECS", "GIN", "GINS", "SG", "SGS"]:
+        # for m in ["EC", "ECS", "GIN", "GINS", "SG", "SGS"]:
+        for m in ["SGS"]:
 
             # Setup model.
             device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -314,7 +318,7 @@ for i in [0, 2, 4, 6, 8, 10]:
 
             # Prepare data.
             bias_threshold = bias
-            batch_size = 15
+            batch_size = 10
             num_epochs = 30
 
 
