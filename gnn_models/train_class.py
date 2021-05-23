f05b519d90901153b811e41d0439bdca41f72c3b
Christopher Morris <christopher.morris@udo.edu>
2021-05-23 01:42:46 -0400

    Complete set of 180 models for GIPS.

--- gnn_models/train_class.py
+++ gnn_models/train_class.py
@@ -271,7 +271,7 @@ name_list = [
 test_scores = []
 
 # Datasets.
-for i in [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]:
+for i in [0, 2, 4, 6, 8, 10]:
 #for i in [12, 14, 16, 18]:
     # Bias.
     for bias in [0.0, 0.001, 0.1]:
