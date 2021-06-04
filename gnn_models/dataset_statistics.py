145b5949383072b19fb43c4e15b4a0eae89d91d5
Christopher Morris <christopher.morris@udo.edu>
2021-06-04 15:48:16 -0400

    DS statistics script. + Fix

--- gnn_models/dataset_statistics.py
+++ gnn_models/dataset_statistics.py
@@ -53,9 +53,9 @@ dataset_list = [
 ]
 
 dataset_list = [
-    "../data_new/data_graphsonly/fcmnf/L_n200_p0.02_c500/train/",
+    #"../data_new/data_graphsonly/fcmnf/L_n200_p0.02_c500/train/",
     #"../data_new/data_graphsonly/gisp/gen200_p0.9_55.clq/train/",
-    #"../data_new/data_graphsonly/gisp/gen200_p0.9_44.clq/train/",
+    "../data_new/data_graphsonly/gisp/gen200_p0.9_44.clq/train/",
 ]
 
 
