7b2634641dd044d0ea643342a16354f8d2b00cf3
Christopher Morris <christopher.morris@udo.edu>
2021-06-04 13:11:46 -0400

    DS statistics script.

--- gnn_models/dataset_statistics.py
+++ gnn_models/dataset_statistics.py
@@ -57,10 +57,10 @@ for i in range(11):
         graph = nx.convert_node_labels_to_integers(graph)
         graph = graph.to_directed() if not nx.is_directed(graph) else graph
 
-        SG_var = graph.subgraph([n for n, attrdict in graph.node.items() if attrdict
+        SG_var = graph.subgraph([n for n, attrdict in graph.nodes.items() if attrdict
         ['bipartite'] == '0'])
 
-        SG_con = graph.subgraph([n for n, attrdict in graph.node.items() if attrdict
+        SG_con = graph.subgraph([n for n, attrdict in graph.nodes.items() if attrdict
         ['bipartite'] == '1'])
 
         num_vars_nodes += SG_var.number_of_nodes()
