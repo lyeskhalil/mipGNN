810df5af347026075ef531a84c9f34fb69b86530
Christopher Morris <christopher.morris@udo.edu>
2021-06-04 14:00:00 -0400

    DS statistics script. + Fix

--- gnn_models/dataset_statistics.py
+++ gnn_models/dataset_statistics.py
@@ -58,16 +58,13 @@ for i in range(11):
         graph = nx.convert_node_labels_to_integers(graph)
         graph = graph.to_directed() if not nx.is_directed(graph) else graph
 
-        for node, node_data in graph.nodes(data=True):
-            print(node_data['bipartite'] == '0')
-        exit()
 
-        SG_var = [i for node, node_data in graph.nodes(data=True) if node_data['bipartite'] == '0']
-        SG_con = [i for node, node_data in graph.nodes(data=True) if node_data['bipartite'] == '1']
+        SG_var = [i for node, node_data in graph.nodes(data=True) if node_data['bipartite'] == 0]
+        SG_con = [i for node, node_data in graph.nodes(data=True) if node_data['bipartite'] == 1]
 
         num_vars_nodes += len(SG_var)
         num_cons_nodes += len(SG_con)
-        print(num_vars_nodes)
+       
 
         num_edges += graph.number_of_edges()
 
